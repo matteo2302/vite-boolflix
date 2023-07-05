f30e257c642bf612236900b12774b9e0b0dfaec1
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-05 18:07:49 +0200

    filled?

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script >
-let endpoint = `https://api.themoviedb.org/3/search//api-key=c07677e7edac90efcbbdd18fecf0d93c`;
+<script>
 import AppSearch from './AppSearch.vue';
 import AppMain from './AppMain.vue';
-import { store } from './data/store.js'
+import { store } from './data/store.js';
 import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -12,33 +12,33 @@ export default {
       movie: 'movie',
       series: 'tv'
     }
-
   },
   components: { AppSearch, AppMain },
+  // methods() {
+  //   // monto l'endpoint con il parametro dinamico
+  //   const filteredUriFilms = `${this.baseUri}/search/movie?api_key=${this.api_key}&query=${string}&language=it-IT`
+  //   const filteredUriSeries = `${this.baseUri}/search/tv?api_key=${this.api_key}&query=${string}&language=it-IT`
+
+  //   console.log(filteredUriFilms)
+  //   console.log(filteredUriSeries)
+  //   // richiamo la funzione che ha la call usando l'endpoint nuovo
+  //   this.fetchFilms(filteredUriFilms)
+  //   this.fetchSeries(filteredUriSeries)
+  // },
   created() {
     let searchType = this.movie;
-    let endpoint = `https://api.themoviedb.org/3/search/${searchType}?api_key=c07677e7edac90efcbbdd18fecf0d93c`;
-    axios.get(endpoint, {
-      params: {
-        query: this.query,
-        language: this.language
-      }
-    }).then((res) => {
+    let endpoint = `https://api.themoviedb.org/3/search/${searchType}?api_key=c07677e7edac90efcbbdd18fecf0d93c&query=anelli&language=it-IT`;
+    axios.get(endpoint).then((res) => {
       store.movies = res.data.results;
       console.log(res.data);
-      console.log(store.movies);
+
     });
     searchType = this.series;
-    endpoint = `https://api.themoviedb.org/3/search/${searchType}?api_key=c07677e7edac90efcbbdd18fecf0d93c`;
-    axios.get(endpoint, {
-      params: {
-        query: this.query,
-        language: this.language
-      }
-    }).then((res) => {
+    endpoint = `https://api.themoviedb.org/3/search/${searchType}?api_key=c07677e7edac90efcbbdd18fecf0d93c&query=anelli&language=it-IT`;
+    axios.get(endpoint).then((res) => {
       store.series = res.data.results;
       console.log(res.data);
-      console.log(store.series);
+
     });
   }
 }
