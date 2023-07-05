c219721a5afd7a7f80a9e1f6ed7d0695ac4ef0a4
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-05 16:33:31 +0200

    filling the store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <script >
-const endpointMovie = 'https://api.themoviedb.org/3/search/movie';
-const endpointSeries = 'https://api.themoviedb.org/3/search/tv';
+let endpoint = 'https://api.themoviedb.org/3/search//api-key=c07677e7edac90efcbbdd18fecf0d93c';
+
 import { store } from './data/store.js'
 import axios from 'axios';
 export default {
-  computed: {}
+  data() {
+    return {
+      query: '',
+      language: 'it-IT',
+      movie: 'movie',
+      series: 'tv'
+    }
+
+  },
+  created() {
+    let searchType = this.movie;
+    endpoint = `https://api.themoviedb.org/3/search/${searchType}?api_key=c07677e7edac90efcbbdd18fecf0d93c`;
+
+    axios.get(endpoint, {
+      params: {
+        query: this.query,
+        language: this.language
+      }
+    }).then((res) => {
+      store.movies = res.data.results
+      console.log(res.data);
+      console.log(store.movies);
+    })
+  }
 }
 </script>
 
