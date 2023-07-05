f92e77b83642f29fae16cdbb6f553ef7b26a2963
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-05 19:18:06 +0200

    series list

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -1,14 +1,17 @@
 <script>
 import AppCard from './AppCard.vue'
+import AppCards from './AppCards.vue'
 export default {
   components: {
-    AppCard
+    AppCard,
+    AppCards
   }
 }
 </script>
 <template>
   <div class="container">
     <AppCard />
+    <AppCards />
   </div>
 </template>
 <style scoped></style>
---
+++ src/appCards.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from './data/store'
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+<template>
+  <div class="card" style="width: 18rem;">
+    <ul class="list-group list-group-flush">
+      <li v-for="serie in store.series" class="list-group-item" :key="serie.id">
+        <div>{{ serie.name }}</div>
+        <div>{{ serie.original_name }}</div>
+        <div>{{ serie.original_language }} </div>
+        <div>{{ serie.vote_average }}</div>
+      </li>
+
+    </ul>
+  </div>
+</template>
+<style scoped></style>
