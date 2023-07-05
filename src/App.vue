401b1334f3b5f702cba11f31e9e1739885fe53b2
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-05 15:48:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script >
+const endpointMovie = 'https://api.themoviedb.org/3/search/movie';
+const endpointSeries = 'https://api.themoviedb.org/3/search/tv';
+import { store } from './data/store.js'
+import axios from 'axios';
+export default {
+  computed: {}
+}
+</script>
+
+<template></template>
+
+<style></style>
