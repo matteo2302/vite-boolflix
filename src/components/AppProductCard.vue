b5597702bc4c6a38cdd7be025d10cedb90200153
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-06 17:04:42 +0200

    poster still not working

--- src/components/AppProductCard.vue
+++ src/components/AppProductCard.vue
@@ -16,7 +16,8 @@ export default {
       return Math.ceil(this.product.vote_average / 2)
     },
     poster() {
-      const url = new URL(`./w780${this.product.backdrop_path}`, import.meta.url);
+      const url = new URL(`./w780${this.product.poster_path}`, import.meta.url);
+      console.log(this.product.poster_path)
       return url.href
     }
   }
@@ -25,7 +26,7 @@ export default {
 <template>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">
-      <div><img :src="poster" :alt="product.name"></div>
+      <div><img :src=`./w780${this.product.poster_path}` :alt="product.name"></div>
       <div>{{ product.title || product.name }}</div>
       <div>{{ product.original_title || product.name }}</div>
       <div><img v-if="hasFlag" :src="flagSrc" :alt="product.original_language">
